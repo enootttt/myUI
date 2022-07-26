4df5eef072008d9a0c6511154678d1167bb61fe9
晚不安 <10671482+asd1232rq3123@user.noreply.gitee.com>
2022-07-26 11:48:02 +0800

    feat: finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
 module.exports = {
+  title: 'My-UI',
+  base: /myUI/,
   themeConfig: {
+    nav: [{
+      text: 'Home', link: '/'
+    },{
+      text: 'Github', link: 'https://github.com/test-921/myUI'
+    },{
+      text: 'Npm', link: 'https://www.npmjs.com/package/myadd-ui'
+    }],
     sidebar: [
       '/',
       '/componentDocs/card'
